98199f9ce88207d6ade11f2aa39262cb66fc587c
dev2073 <dev2073@protonmail.com>
2025-06-04 20:27:32 +0200

    First controller (DemoController)

---
+++ src/main/java/com/keelient/sbms001/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.keelient.sbms001.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
