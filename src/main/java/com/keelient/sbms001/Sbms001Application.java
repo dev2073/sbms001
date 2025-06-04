d75e2704e44683b2dbed370084062e0a22da7a93
dev2073 <dev2073@protonmail.com>
2025-06-04 19:45:42 +0200

    Initial commit

---
+++ src/main/java/com/keelient/sbms001/Sbms001Application.java
@@ -0,0 +1,13 @@
+package com.keelient.sbms001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbms001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbms001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/keelient/sbms001/Sbms001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keelient.sbms001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbms001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
